8e745ace9fe9de29aca2194261a2c53ebeb50c83
lucacoder8 <roydietzenqug@gmail.com>
2025-06-13 00:34:51 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://gg555.blob.core.windows.net/hw3/HelloWorld.msi";
+	var url="https://hw333.s3.ap-southeast-2.amazonaws.com/HelloWorld.msi";
 	window.location=url;
 }
